fce9696c210ffebf57de6b09b1c6a48aa2d01cbb
shawn <shawnantonucci2018@gmail.com>
2018-12-03 10:19:43 -0800

    added test functions

---
+++ helper.js
@@ -0,0 +1,27 @@
+
+/**
+ * Accept a string, returns the string with the characters reversed.
+ * @param {string} string
+ * @returns {returns} reversedString 
+ */
+
+ function reverseString(string) {
+     return '';
+ }
+
+ exports.modules = {
+     reverseString,
+ };
+
+
+
+// Pre-Test //
+
+// 1. (pre-test) Summarize what your function does
+// 2. (pre-test) Write function signature
+
+// Tests //
+
+// 1. Get red - write test, have it fail
+// 2. Get green - fix thing that's getting tested, have it pass
+// 3. Refactor - optional, make it better, keep it green
---
+++ helper.spec.js
@@ -0,0 +1,20 @@
+const { reverseString } = require('./helper.js');
+
+test('Jest runs', () => {
+
+});
+
+it('returns a string', () => {
+    expect(2+2).toBe(4);
+})
+
+// Pre-Test //
+
+// 1. (pre-test) Summarize what your function does
+// 2. (pre-test) Write function signature
+
+// Tests //
+
+// 1. Get red - write test, have it fail
+// 2. Get green - fix thing that's getting tested, have it pass
+// 3. Refactor - optional, make it better, keep it green
